7286e3dbc98e60e72a2f7d858365475580522475
Fadlu <fadlushakir@gmail.com>
2026-06-22 12:19:11 +0530

    Auto-create superuser script

--- backend_clean/build.sh
+++ backend_clean/build.sh
@@ -6,3 +6,12 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Automatically create a superuser if environment variables are provided
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email admin@resort.com || true
+    echo "Superuser creation attempted."
+fi
