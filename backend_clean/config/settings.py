2de10526c943b1da96368bb2889a677fd7581b38
Fadlu <fadlushakir@gmail.com>
2026-06-23 11:04:47 +0530

    Fix: Migrate Cloudinary and WhiteNoise storage configuration to modern STORAGES dictionary for Django 5.1+

--- backend_clean/config/settings.py
+++ backend_clean/config/settings.py
@@ -92,11 +92,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME', 'dummy-cloud-name'),
